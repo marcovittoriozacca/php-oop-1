ba98074efbd59f9a3fc9e5cb787e297f41c56874
marcovittoriozacca <zaccamarco01@gmail.com>
2024-03-07 11:29:22 +0100

    feat: Class with 2 instances + variables + methods and construct

--- index.php
+++ index.php
@@ -1,5 +1,44 @@
 <?php 
 
+//classe Movie
+class Movie{
+    //variabili di classe
+    public $title;
+    public $releaseDate;
+    public $director;
+    public $starring;
+    public $genres;
+
+    //costruttore
+    function __construct($_title, $_releaseDate, $_director, $_starring, $_genres){
+        $this->title = $_title;
+        $this->releaseDate = $_releaseDate;
+        $this->director = $_director;
+        $this->starring = $_starring;
+        $this->genres = $_genres;
+    }
+
+    //metodi/funzioni di classe
+    public function changeTitle($newTitle){
+        return $this->title = $newTitle;
+    }
+
+    public function changeReleaseDate($newReleaseDate){
+        return $this->releaseDate = $newReleaseDate;
+    }
+    public function changeDirector($newDirector){
+        return $this->director = $newDirector;
+    }
+
+}
+
+//creazione istanza della classe Movie
+$movie1 = new Movie('The Godfather', 1972, 'Francis Ford Coppola', 'actors', 'genres');
+$movie2 = new Movie('Titanic', 1997, 'James Cameron', 'actors', 'genres');
+
+
+var_dump($movie1, $movie2);
+
 ?>
 
 <!DOCTYPE html>
@@ -11,5 +50,17 @@
 </head>
 <body>
     
+    <?php 
+    
+        $movie1->changeTitle('Pulp Fiction');
+
+        $movie1->changeReleaseDate(1994);
+
+        $movie1->changeDirector('Quentin Tarantino');
+
+        var_dump($movie1);
+
+    ?>
+
 </body>
 </html>
